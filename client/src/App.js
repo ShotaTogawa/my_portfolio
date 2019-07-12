86367c23ecfa79762dfd8d8bd310d9534886f622
ShotaTogawa <s.togawa0112@gmail.com>
2019-07-11 19:21:48 -0700

    installed redux packages and import packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import firebase from './firebase';
 
 class App extends Component {
+  
   render() {
     return (
       <div>
-        <Login />
+        Hello world
       </div>
     );
   }
